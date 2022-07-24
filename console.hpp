5fc55a834569f970b53322d28ce0ea415cffb317
andrey moura <andreymds88@gmail.com>
2022-07-24 15:20:15 -0300

    New line processing

--- console.hpp
+++ console.hpp
@@ -20,7 +20,7 @@ namespace uva
             pink = 35,
             light_blue = 36
 
-        };
+        }; 
 
         struct color
         {
@@ -35,23 +35,44 @@ namespace uva
             // Override for std::cout compatibility - that finishs the color code on console
             friend std::ostream& operator << (const color& color, const std::string& str)
             {
-                std::cout << str;
-
                 std::ostream& os = *color.os;
 
-                os << "\033[0m";
+                for(const char& c : str) {
 
+                    os << c;
+
+                    if(c == '\n') {
+                        color.end_color();
+                        color.begin_color();
+                    }
+
+                }
+
+                color.end_color();
+
+                return os;
+            }
+
+            std::string begin_color() const
+            {
+                *os << "\033[" << (size_t)code << "m";
+            }
+
+            std::string end_color() const 
+            {
+                *os << "\033[0m";
+            }
+
+            // Override for std::cout compatibility - that starts color code on console
+            friend std::ostream& operator << (std::ostream& os, const color& c)
+            {
+                color* pColor = (color*)&c;
+                pColor->os = &os;
+
+                c.begin_color();
                 return os;
             }
         };
 
-        // Override for std::cout compatibility - that finishs starts color code on console
-        std::ostream& operator << (std::ostream& os, const color& c)
-        {
-            os << "\033[" << (size_t)c.code << "m";
-            color* pColor = (color*)&c;
-            pColor->os = &os;
-            return os;
-        }
     };
 };
